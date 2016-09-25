1d7e3f97d81b5d80791cda62051eed7eb3076fa2
The_Fireplace <the.f1repl4ce@gmail.com>
2016-09-24 20:37:33 -0500

    Minor performance improvement

--- src/main/java/the_fireplace/skeletonwars/items/ItemSansMask.java
+++ src/main/java/the_fireplace/skeletonwars/items/ItemSansMask.java
@@ -1,9 +1,12 @@
 package the_fireplace.skeletonwars.items;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import the_fireplace.skeletonwars.SkeletonWars;
 
 /**
@@ -18,5 +21,11 @@ public class ItemSansMask extends ItemArmor {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
         return SkeletonWars.MODID+":textures/armor/sans.png";
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public CreativeTabs getCreativeTab(){
+        return null;
     }
 }
