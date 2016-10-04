11b2da6c417361647c4a3658e32f520481b43362
The_Fireplace <the.f1repl4ce@gmail.com>
2016-10-03 21:20:04 -0500

    Fixed a bug with an easter egg

--- src/main/java/the_fireplace/overlord/Overlord.java
+++ src/main/java/the_fireplace/overlord/Overlord.java
@@ -205,6 +205,6 @@ public class Overlord {
         heya.registerStat();
         heya.setSpecial();
         AchievementPage.registerAchievementPage(new AchievementPage(MODNAME,
-                firstSkeleton, secondSkeleton, firstLevel, firstMilk, armedSkeleton, sally, crusader, milk256, milk9001, nmyi, alliance, breakalliance));
+                firstSkeleton, secondSkeleton, firstLevel, firstMilk, armedSkeleton, sally, crusader, milk256, milk9001, nmyi, alliance, breakalliance, heya));
     }
 }
