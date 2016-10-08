9ee3b6948192587a3973cb95edf05e2411725e20
The_Fireplace <the.f1repl4ce@gmail.com>
2016-10-08 11:24:06 -0500

    Play a sound when receiving an AI update. Closes #20

--- src/main/java/the_fireplace/overlord/entity/EntitySkeletonWarrior.java
+++ src/main/java/the_fireplace/overlord/entity/EntitySkeletonWarrior.java
@@ -115,6 +115,7 @@ public class EntitySkeletonWarrior extends EntityMob implements IEntityOwnable {
         if(this.dataManager.get(ATTACK_MODE) != 0)
             addAttackTasks();
         addTargetTasks();
+        this.playSound(getAmbientSound(), getSoundVolume()*1.5F, getSoundPitch()*1.5F);
     }
 
     public void addMovementTasks(){
