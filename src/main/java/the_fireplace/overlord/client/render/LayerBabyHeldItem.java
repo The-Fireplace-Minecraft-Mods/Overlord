a4a7e7da1b0ddedbd3393431c965dd346124ef89
The_Fireplace <the.f1repl4ce@gmail.com>
2016-12-13 20:01:27 -0600

    Fixed Baby Skeleton held item rendering

---
+++ src/main/java/the_fireplace/overlord/client/render/LayerBabyHeldItem.java
@@ -0,0 +1,74 @@
+package the_fireplace.overlord.client.render;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHandSide;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@SideOnly(Side.CLIENT)
+@ParametersAreNonnullByDefault
+public class LayerBabyHeldItem implements LayerRenderer<EntityLivingBase>
+{
+    protected final RenderLivingBase<?> livingEntityRenderer;
+
+    public LayerBabyHeldItem(RenderLivingBase<?> livingEntityRendererIn)
+    {
+        this.livingEntityRenderer = livingEntityRendererIn;
+    }
+
+    @Override
+    public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+    {
+        boolean flag = entitylivingbaseIn.getPrimaryHand() == EnumHandSide.RIGHT;
+        ItemStack itemstack = flag ? entitylivingbaseIn.getHeldItemOffhand() : entitylivingbaseIn.getHeldItemMainhand();
+        ItemStack itemstack1 = flag ? entitylivingbaseIn.getHeldItemMainhand() : entitylivingbaseIn.getHeldItemOffhand();
+
+        if (itemstack != null || itemstack1 != null)
+        {
+            GlStateManager.pushMatrix();
+
+            GlStateManager.translate(0.0F, 0.625F, 0.0F);
+            GlStateManager.rotate(-20.0F, -1.0F, 0.0F, 0.0F);
+            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+
+            this.renderHeldItem(entitylivingbaseIn, itemstack1, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, EnumHandSide.RIGHT);
+            this.renderHeldItem(entitylivingbaseIn, itemstack, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, EnumHandSide.LEFT);
+            GlStateManager.popMatrix();
+        }
+    }
+
+    private void renderHeldItem(EntityLivingBase entityLivingBase, ItemStack itemStack, ItemCameraTransforms.TransformType transformType, EnumHandSide handSide)
+    {
+        if (itemStack != null)
+        {
+            GlStateManager.pushMatrix();
+
+            if (entityLivingBase.isSneaking())
+            {
+                GlStateManager.translate(0.0F, 0.2F, 0.0F);
+            }
+            ((ModelBiped)this.livingEntityRenderer.getMainModel()).postRenderArm(0.0625F, handSide);
+            GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+            boolean flag = handSide == EnumHandSide.LEFT;
+            GlStateManager.translate((float)(flag ? -1 : 1) / 16.0F, 0.125F, -0.625F);
+            Minecraft.getMinecraft().getItemRenderer().renderItemSide(entityLivingBase, itemStack, transformType, flag);
+            GlStateManager.popMatrix();
+        }
+    }
+
+    @Override
+    public boolean shouldCombineTextures()
+    {
+        return false;
+    }
+}
