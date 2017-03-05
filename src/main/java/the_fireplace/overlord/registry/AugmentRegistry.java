22e788be75973598875cc91c14cf6d2b1e060d79
The_Fireplace <the.f1repl4ce@gmail.com>
2017-03-05 14:36:50 -0600

    augment registry multiple items

--- src/main/java/the_fireplace/overlord/registry/AugmentRegistry.java
+++ src/main/java/the_fireplace/overlord/registry/AugmentRegistry.java
@@ -5,53 +5,53 @@ import net.minecraft.item.ItemStack;
 import the_fireplace.overlord.Overlord;
 import the_fireplace.overlord.tools.Augment;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.HashMap;
 
 /**
  * @author The_Fireplace
  */
+@ParametersAreNonnullByDefault
 public abstract class AugmentRegistry {
-    private static HashMap<ItemStack, Augment> augments = Maps.newHashMap();
+    private static HashMap<ItemStack, String> augments = Maps.newHashMap();
     private static HashMap<String, Augment> augmentIDs = Maps.newHashMap();
 
-    public static boolean registerAugment(@Nonnull ItemStack item, @Nonnull Augment augment){
+    public static boolean registerAugment(ItemStack item, Augment augment){
         if(augment.augmentId().isEmpty()) {
             Overlord.logError("Augment "+augment.getClass()+" has no ID, skipping...");
             return false;
         }
-        if(!augmentIDs.keySet().contains(augment.augmentId())) {
-            item.stackSize=1;
-            if (!augments.containsKey(item)) {
-                augments.put(item, augment);
-                augmentIDs.put(augment.augmentId(), augment);
-                return true;
-            } else {
-                Overlord.logWarn("Augment already exists for " + item.getItem() + ", skipping...");
-                return false;
-            }
-        }else{
-            Overlord.logError("Augment already exists for ID "+augment.augmentId()+", skipping "+augment.getClass()+"...");
+        if (!augments.containsKey(item)) {
+            item.setCount(1);
+            augments.put(item, augment.augmentId());
+        } else {
+            Overlord.logError("Augment already exists for " + item.getItem() + ", skipping...");
             return false;
         }
+        if(!augmentIDs.keySet().contains(augment.augmentId())) {
+            augmentIDs.put(augment.augmentId(), augment);
+        }else{
+            Overlord.logWarn("Augment already exists for ID "+augment.augmentId()+", skipping "+augment.getClass()+"...");
+        }
+        return true;
     }
 
     @Nullable
     public static Augment getAugment(ItemStack stack){
-        if(stack == null)
+        if(stack.isEmpty())
             return null;
         ItemStack stack1 = stack.copy();
-        stack1.stackSize=1;
-        for(ItemStack augment:augments.keySet()){
-            if(ItemStack.areItemStacksEqual(stack1, augment))
-                return augments.get(augment);
+        stack1.setCount(1);
+        for(ItemStack augmentItem:augments.keySet()){
+            if(ItemStack.areItemStacksEqual(stack1, augmentItem))
+                return getAugment(augments.get(augmentItem));
         }
         return null;
     }
 
     @Nullable
-    public static Augment getAugment(String id){
+    public static Augment getAugment(@Nullable String id){
         if(id == null || id.isEmpty())
             return null;
         else
