1a32d9bcad2636f654760965ffa3282809557c08
The_Fireplace <the.f1repl4ce@gmail.com>
2017-03-21 14:01:27 -0500

    Should fix command permissions. Closes #78

--- src/main/java/the_fireplace/overlord/command/CommandAllyList.java
+++ src/main/java/the_fireplace/overlord/command/CommandAllyList.java
@@ -28,6 +28,11 @@ public class CommandAllyList extends CommandBase {
     }
 
     @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return sender instanceof EntityPlayer;
+    }
+
+    @Override
     public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) throws CommandException {
         if (sender instanceof EntityPlayer) {
             if(!Alliances.getInstance().getAllies(((EntityPlayer) sender).getUniqueID()).isEmpty()) {
--- src/main/java/the_fireplace/overlord/command/CommandEnemyRemove.java
+++ src/main/java/the_fireplace/overlord/command/CommandEnemyRemove.java
@@ -30,6 +30,11 @@ public class CommandEnemyRemove extends CommandBase {
     }
 
     @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return sender instanceof EntityPlayer;
+    }
+
+    @Override
     public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) throws CommandException {
         if (sender instanceof EntityPlayer) {
             if(args.length == 1){
