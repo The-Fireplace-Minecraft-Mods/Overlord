f864caaf0fbe72479153fae92a7ad0430bc49086
The_Fireplace <the.f1repl4ce@gmail.com>
2017-03-31 16:50:49 -0500

    Moved the augment rendering to a separate class

--- src/main/java/the_fireplace/overlord/client/render/RenderSkeletonWarrior.java
+++ src/main/java/the_fireplace/overlord/client/render/RenderSkeletonWarrior.java
@@ -1,17 +1,9 @@
 package the_fireplace.overlord.client.render;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -19,9 +11,9 @@ import the_fireplace.overlord.Overlord;
 import the_fireplace.overlord.entity.EntitySkeletonWarrior;
 import the_fireplace.overlord.network.PacketDispatcher;
 import the_fireplace.overlord.network.packets.RequestAugmentMessage;
+import the_fireplace.overlord.tools.RenderTools;
 
 import javax.annotation.Nonnull;
-import java.util.Random;
 
 /**
  * @author The_Fireplace
@@ -35,9 +27,6 @@ public class RenderSkeletonWarrior extends RenderBiped<EntitySkeletonWarrior>
     public static final ResourceLocation OBSIDIAN_SKELETON_TEXTURES = new ResourceLocation(Overlord.MODID, "textures/entity/obsidian_skeleton.png");
     public static final ResourceLocation ANVIL_SKELETON_TEXTURES = new ResourceLocation(Overlord.MODID, "textures/entity/anvil_skeleton.png");
     public static final ResourceLocation BRAINSTONE_SKELETON_TEXTURES = new ResourceLocation(Overlord.MODID, "textures/entity/brainstone_skeleton.png");
-
-    static Random random = new Random();
-    EntityItem entityItem;
 
     public RenderSkeletonWarrior(RenderManager renderManagerIn)
     {
@@ -88,138 +77,8 @@ public class RenderSkeletonWarrior extends RenderBiped<EntitySkeletonWarrior>
         super.doRender(entitySkeletonWarrior, x, y, z, entityYaw, partialTicks);
 
         //Render item
-        if(entityItem == null) {
-            entityItem = new EntityItem(entitySkeletonWarrior.world, x, y + entitySkeletonWarrior.height + 0.25F, z, entitySkeletonWarrior.getAugmentDisplayStack());
-            entityItem.setNoDespawn();
-        }
-        entityItem.setLocationAndAngles(x, y+entitySkeletonWarrior.height+0.25F, z, 0, 0);
-        entityItem.setEntityItemStack(entitySkeletonWarrior.getAugmentDisplayStack());
-
-        ItemStack itemstack = entityItem.getEntityItem();
-        int i = itemstack.isEmpty() ? 187 : Item.getIdFromItem(itemstack.getItem()) + itemstack.getMetadata();
-        random.setSeed((long)i);
-
-        bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        this.renderManager.renderEngine.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
-
-        GlStateManager.enableRescaleNormal();
-        GlStateManager.alphaFunc(516, 0.1F);
-        GlStateManager.enableBlend();
-        RenderHelper.enableStandardItemLighting();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.pushMatrix();
-        IBakedModel ibakedmodel = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(itemstack, entityItem.world, null);
-        int j = this.transformModelCount(entityItem, x, y, z, partialTicks, ibakedmodel);
-        boolean flag1 = ibakedmodel.isGui3d();
-
-        if (!flag1)
-        {
-            float f3 = -0.0F * (float)(j - 1) * 0.5F;
-            float f4 = -0.0F * (float)(j - 1) * 0.5F;
-            float f5 = -0.09375F * (float)(j - 1) * 0.5F;
-            GlStateManager.translate(f3, f4, f5);
-        }
-
-        GlStateManager.translate(0, entitySkeletonWarrior.height+0.1F, 0);
-
-        for (int k = 0; k < j; ++k)
-        {
-            if (flag1)
-            {
-                GlStateManager.pushMatrix();
-
-                if (k > 0)
-                {
-                    float f7 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    float f9 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    float f6 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F;
-                    GlStateManager.translate(f7, f9, f6);
-                }
-
-                ibakedmodel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(ibakedmodel, ItemCameraTransforms.TransformType.GROUND, false);
-                Minecraft.getMinecraft().getRenderItem().renderItem(itemstack, ibakedmodel);
-                GlStateManager.popMatrix();
-            }
-            else
-            {
-                GlStateManager.pushMatrix();
-
-                if (k > 0)
-                {
-                    float f8 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
-                    float f10 = (random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
-                    GlStateManager.translate(f8, f10, 0.0F);
-                }
-
-                ibakedmodel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(ibakedmodel, ItemCameraTransforms.TransformType.GROUND, false);
-                Minecraft.getMinecraft().getRenderItem().renderItem(itemstack, ibakedmodel);
-                GlStateManager.popMatrix();
-                GlStateManager.translate(0.0F, 0.0F, 0.09375F);
-            }
-        }
-
-        if (this.renderOutlines)
-        {
-            GlStateManager.disableOutlineMode();
-            GlStateManager.disableColorMaterial();
-        }
-
-        GlStateManager.popMatrix();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.disableBlend();
-        bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-
-        this.renderManager.renderEngine.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-    }
-
-    private int transformModelCount(EntityItem itemIn, double p_177077_2_, double p_177077_4_, double p_177077_6_, float p_177077_8_, IBakedModel p_177077_9_)
-    {
-        ItemStack itemstack = itemIn.getEntityItem();
-        Item item = itemstack.getItem();
-
-        if (item == null)
-        {
-            return 0;
-        }
-        else
-        {
-            boolean flag = p_177077_9_.isGui3d();
-            int i = this.getModelCount(itemstack);
-            float f2 = p_177077_9_.getItemCameraTransforms().getTransform(ItemCameraTransforms.TransformType.GROUND).scale.y;
-            GlStateManager.translate((float)p_177077_2_, (float)p_177077_4_ + 0.25F * f2, (float)p_177077_6_);
-
-            if (flag || this.renderManager.options != null)
-            {
-                float f3 = (((float)itemIn.getAge() + p_177077_8_) / 20.0F + itemIn.hoverStart) * (180F / (float)Math.PI);
-                GlStateManager.rotate(f3, 0.0F, 1.0F, 0.0F);
-            }
-
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            return i;
-        }
-    }
-
-    protected int getModelCount(ItemStack stack)
-    {
-        int i = 1;
-
-        if (stack.getCount() > 48)
-        {
-            i = 5;
-        }
-        else if (stack.getCount() > 32)
-        {
-            i = 4;
-        }
-        else if (stack.getCount() > 16)
-        {
-            i = 3;
-        }
-        else if (stack.getCount() > 1)
-        {
-            i = 2;
-        }
-
-        return i;
+        if(!entitySkeletonWarrior.cachedClientAugment)
+            PacketDispatcher.sendToServer(new RequestAugmentMessage(entitySkeletonWarrior));
+        RenderTools.renderItemStackOverEntity(entitySkeletonWarrior, entitySkeletonWarrior.getAugmentDisplayStack(), this, partialTicks);
     }
 }
