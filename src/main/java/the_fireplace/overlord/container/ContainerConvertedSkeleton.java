a0569e3b373638f53c453d4495d86ecad0b2d315
The_Fireplace <the.f1repl4ce@gmail.com>
2017-04-01 11:41:31 -0500

    fix shift click augment con skelly

--- src/main/java/the_fireplace/overlord/container/ContainerConvertedSkeleton.java
+++ src/main/java/the_fireplace/overlord/container/ContainerConvertedSkeleton.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import the_fireplace.overlord.entity.EntityConvertedSkeleton;
 import the_fireplace.overlord.network.PacketDispatcher;
 import the_fireplace.overlord.network.packets.RequestAugmentMessage;
+import the_fireplace.overlord.registry.AugmentRegistry;
 
 import javax.annotation.Nonnull;
 
@@ -105,6 +106,8 @@ public class ContainerConvertedSkeleton extends Container {
                 if (!mergeItemStack(is, 0, 36, false)) {
                     return ItemStack.EMPTY;
                 }
+            } else if (AugmentRegistry.getAugment(is) != null && !mergeItemStack(is, 36+6, 36 + entity.inventory.getSizeInventory() + entity.equipInventory.getSizeInventory(), false)) {
+                return ItemStack.EMPTY;
             } else if (!mergeItemStack(is, 36, 36 + entity.inventory.getSizeInventory() + entity.equipInventory.getSizeInventory(), false)) {
                 return ItemStack.EMPTY;
             }
