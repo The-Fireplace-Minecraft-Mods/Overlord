02436ebcdcf1981c2554436614e46e45355375da
The_Fireplace <the.f1repl4ce@gmail.com>
2020-02-20 15:20:10 -0600

    Work on ai settings

--- fabric/src/main/java/the_fireplace/overlord/fabric/model/AISettings.java
+++ fabric/src/main/java/the_fireplace/overlord/fabric/model/AISettings.java
@@ -11,6 +11,7 @@ public class AISettings {
     private boolean combat = true;
     private boolean movement = true;
     private boolean tasks = true;
+
     //Combat strategies
     private boolean melee = true;
     private boolean ranged = true;
@@ -19,10 +20,46 @@ public class AISettings {
     //Melee settings
     private boolean switchToRangedWhenFar = true;
     private byte rangedSwitchDistance = 4;
-    //Bow settings
+    private TargetEntitySelector meleeTargets;
+    //Ranged settings
     private boolean switchToMeleeWhenNoArrows = true;
     private boolean switchToMeleeWhenClose = true;
     private byte meleeSwitchDistance = 3;
+    private TargetEntitySelector rangedTargets;
     //Throw Item settings
     private UUID throwItemList = AIListManager.ALL_THROWABLES_LIST_ID;
+    private TargetEntitySelector throwTargets;
+
+    //Movement modes
+    private EnumMovementMode moveMode = EnumMovementMode.STATIONED;
+    //How close the skeleton follows the player
+    private byte followDistance = 8;
+    private UUID patrolPosList = AIListManager.EMPTY_LIST_ID;
+    private Pos home = null;
+    //Should the stationed skeleton return to the home position after walking away to attack
+    private boolean stationedReturnHome = true;
+    //The radius away from home the skeleton can go when attacking while stationed or wandering the area
+    private byte moveRadius = 16;
+
+    //Tasks
+    private boolean woodcutting = false;
+    private boolean mining = false;
+    private boolean digging = false;
+    private boolean beekeeping = false;
+    private boolean farming = false;
+    private boolean building = false;
+    private boolean depositItems = false;
+    private boolean withdrawItems = false;
+    //Woodcutting settings
+    private UUID woodcuttingBlockList = AIListManager.EMPTY_LIST_ID;
+    private boolean woodcuttingWithoutTools = true;
+    //Mining settings
+    private UUID miningBlockList = AIListManager.EMPTY_LIST_ID;
+    private boolean miningWithoutTools = false;
+    //Digging settings
+    private UUID diggingBlockList = AIListManager.EMPTY_LIST_ID;
+    private boolean diggingWithoutTools = true;
+    //Beekeeping settings
+    //Should the skeleton collect honey from hives that don't have smoke to calm them?
+    private boolean harvestAngryHives = false;
 }
---
+++ fabric/src/main/java/the_fireplace/overlord/fabric/model/EnumMovementMode.java
@@ -0,0 +1,9 @@
+package the_fireplace.overlord.fabric.model;
+
+public enum EnumMovementMode {
+    FOLLOW,
+    PATROL,
+    WANDER,
+    EXPLORE,
+    STATIONED
+}
