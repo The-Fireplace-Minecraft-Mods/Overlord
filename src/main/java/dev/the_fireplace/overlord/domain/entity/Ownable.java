994efb6e76391a520b61f0a9633697bbb5906763
The_Fireplace <the.f1repl4ce@gmail.com>
2021-11-20 09:56:24 -0600

    Fix Ownable interface conflict with vanilla functions

--- src/main/java/dev/the_fireplace/overlord/domain/entity/Ownable.java
+++ src/main/java/dev/the_fireplace/overlord/domain/entity/Ownable.java
@@ -1,9 +1,7 @@
 package dev.the_fireplace.overlord.domain.entity;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Tameable;
 
 public interface Ownable extends Tameable
 {
-    boolean canAttackWithOwner(LivingEntity target, LivingEntity owner);
 }
