09ceefd0cba4a2bf2fa27d2ba2269fa73532f125
The_Fireplace <the.f1repl4ce@gmail.com>
2021-11-29 18:16:03 -0600

    Finish basic implementation of Squads interface

--- src/main/java/dev/the_fireplace/overlord/domain/data/Squads.java
+++ src/main/java/dev/the_fireplace/overlord/domain/data/Squads.java
@@ -3,15 +3,21 @@ package dev.the_fireplace.overlord.domain.data;
 import dev.the_fireplace.overlord.domain.data.objects.Squad;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.UUID;
 
 public interface Squads
 {
-    void saveSquad(Squad squad);
-
+    @Nullable
     Squad getSquad(UUID owner, UUID squadId);
 
-    Squad removeSquad(UUID owner, UUID squadId);
+    @Nullable
+    Squad createNewSquad(UUID owner, String capeBase, ItemStack stack, String name);
+
+    void removeSquad(UUID owner, UUID squadId);
+
+    Collection<? extends Squad> getSquadsWithOwner(UUID owner);
 
     boolean isCapeUnused(String capeBase, ItemStack stack);
 
