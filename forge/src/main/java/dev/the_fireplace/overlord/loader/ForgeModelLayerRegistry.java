0d45deac09fe0a05f2e2809481d641ab0418804b
The_Fireplace <git@the-fireplace.dev>
2022-11-16 17:21:47 -0600

    [Forge] Fix server crash
    Closes #239

--- forge/src/main/java/dev/the_fireplace/overlord/loader/ForgeModelLayerRegistry.java
+++ forge/src/main/java/dev/the_fireplace/overlord/loader/ForgeModelLayerRegistry.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-@Implementation
+@Implementation(environment = "CLIENT")
 @Singleton
 public final class ForgeModelLayerRegistry implements ModelLayerRegistry
 {
