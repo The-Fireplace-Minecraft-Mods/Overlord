556bda18e6e6499599af21daf8a0359dd8782e01
The_Fireplace <git@the-fireplace.dev>
2022-11-29 09:51:28 -0600

    [1.19] Fixed skeleton spawn crash
    RandomSource can crash if used from multiple threads, switch to a normal Random

--- common/src/main/java/dev/the_fireplace/overlord/util/SkeletonSpawnUtils.java
+++ common/src/main/java/dev/the_fireplace/overlord/util/SkeletonSpawnUtils.java
@@ -10,20 +10,23 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 public final class SkeletonSpawnUtils
 {
     private final OverlordItems overlordItems;
+    private final Random random;
 
     @Inject
     public SkeletonSpawnUtils(OverlordItems overlordItems) {
         this.overlordItems = overlordItems;
+        this.random = new Random();
     }
 
     public void addMask(LivingEntity entity) {
         int daysSinceUndertaleReleased = (int) ChronoUnit.DAYS.between(LocalDate.parse("2015-09-15", DateTimeFormatter.ofPattern("y-M-d")), LocalDateTime.now());
         int daysSince1710Released = (int) ChronoUnit.DAYS.between(LocalDate.parse("2014-06-26", DateTimeFormatter.ofPattern("y-M-d")), LocalDateTime.now());
-        if (entity.getCommandSenderWorld().getRandom().nextInt(daysSinceUndertaleReleased + daysSince1710Released) == 0
+        if (random.nextInt(daysSinceUndertaleReleased + daysSince1710Released) == 0
             && entity.getItemBySlot(EquipmentSlot.HEAD).isEmpty()
         ) {
             entity.setItemSlot(EquipmentSlot.HEAD, new ItemStack(overlordItems.getSansMask()));
