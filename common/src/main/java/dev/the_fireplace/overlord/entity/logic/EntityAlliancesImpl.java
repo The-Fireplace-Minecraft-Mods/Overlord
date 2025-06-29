adb326802f57c5f5071067739bd91359ad50cfb5
The_Fireplace <git@the-fireplace.dev>
2025-06-28 23:14:34 -0500

    Have entities check player alliances when looking at entity alliance status

--- common/src/main/java/dev/the_fireplace/overlord/entity/logic/EntityAlliancesImpl.java
+++ common/src/main/java/dev/the_fireplace/overlord/entity/logic/EntityAlliancesImpl.java
@@ -1,6 +1,7 @@
 package dev.the_fireplace.overlord.entity.logic;
 
 import dev.the_fireplace.annotateddi.api.di.Implementation;
+import dev.the_fireplace.overlord.domain.data.PlayerAlliances;
 import dev.the_fireplace.overlord.domain.entity.logic.EntityAlliances;
 import dev.the_fireplace.overlord.domain.entity.logic.EntityOwnership;
 import net.minecraft.world.entity.Entity;
@@ -13,18 +14,21 @@ import java.util.UUID;
 public class EntityAlliancesImpl implements EntityAlliances
 {
     private final EntityOwnership entityOwnership;
+    private final PlayerAlliances playerAlliances;
 
     @Inject
-    public EntityAlliancesImpl(EntityOwnership entityOwnership) {
+    public EntityAlliancesImpl(EntityOwnership entityOwnership, PlayerAlliances playerAlliances) {
         this.entityOwnership = entityOwnership;
+        this.playerAlliances = playerAlliances;
     }
 
     @Override
     public boolean isAlliedTo(Entity entity, UUID player) {
         Collection<UUID> owners = entityOwnership.getOwnersOrSelf(entity);
 
-        //TODO player alliance check once that system is in place
-        return owners.contains(player);
+        return owners.stream().anyMatch((entityOwner) -> entityOwner.equals(player)
+            || playerAlliances.hasAllianceWith(player, entityOwner)
+        );
     }
 
     @Override
@@ -32,6 +36,8 @@ public class EntityAlliancesImpl implements EntityAlliances
         Collection<UUID> owners = entityOwnership.getOwnersOrSelf(entity);
         Collection<UUID> otherOwners = entityOwnership.getOwnersOrSelf(otherEntity);
 
-        return owners.stream().anyMatch(otherOwners::contains);
+        return otherOwners.stream().anyMatch((other) ->
+            owners.stream().anyMatch((owner) -> owner.equals(other) || playerAlliances.hasAllianceWith(owner, other))
+        );
     }
 }
